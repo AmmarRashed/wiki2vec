4028ea24d00b7988ebcb3a33e632e38241d1d1c5
Ammar Rashed <ammarrashed@std.sehir.edu.tr>
2018-07-04 14:38:22 +0300

    first commit

---
+++ WordEmbeddings.py
@@ -0,0 +1,101 @@
+import ntpath,os
+import multiprocessing
+
+
+import re
+import time
+
+from gensim.corpora import WikiCorpus
+from joblib import Parallel, delayed  # for parallel computation
+
+import nltk
+from gensim.models.phrases import Phraser, Phrases
+
+class WordEmbeddings(object):
+    def __init__(self, inp, out, model_name="ft_sg",
+                 window=300, min_count=3,
+                 workers=multiprocessing.cpu_count(),
+                 sample=0.001,
+                 negative=5,
+                 seed=48,
+                 lang="en"):
+
+        self.model_name = model_name
+        self.lang = lang
+
+        if "w2v" in model_name:
+            from gensim.models.word2vec import Word2Vec
+            self.model = Word2Vec
+        else:
+            from gensim.models.fasttext import FastText
+            self.model = FastText
+
+        if "sg" in model_name:
+            self.sg = 1
+        else:
+            self.sg = 0
+
+        self.out = out
+        self.inp = inp
+
+        self.window = window
+        self.min_count = min_count
+        self.workers = workers
+        self.sample = sample
+        self.negative = negative
+        self.seed = seed
+
+    @staticmethod
+    def sentence_to_wordlist(raw: str):
+        return re.sub("[^a-zA-Z]", " ", raw).split()
+
+    def clean_wiki(self, output_path="data"):
+        wiki = WikiCorpus(self.inp, lemmatize=False, dictionary={})
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+
+        with open(os.path.join(output_path, ntpath.basename(self.inp)+"_clean"), 'w') as output:
+            n = 0
+            for i, text in enumerate(wiki.get_texts()):
+                # Note: if you're using Python 2, use:
+                # output.write(" ".join(unicode(text)) + "\n")
+                output.write(" ".join(text) + "\n")
+                if i > 0 and i % 10000 == 0:
+                    print("Saved {0:,} articles".format(i))
+                n = i
+        print("Finished saving {0:,} articles".format(n))
+
+        return wiki
+
+    def get_sentences(self):
+        raw_corpus = self.clean_wiki()
+
+        if self.lang == "en":
+            nltk.download("punkt")
+            tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+            raw_sentences = tokenizer.tokenize(raw_corpus.lower())
+        else:
+            raw_sentences = raw_corpus.lower().split(".")
+
+        tokenized_sentences = Parallel(n_jobs=self.workers)(
+            delayed(self.sentence_to_wordlist)(
+                raw_sentence) for raw_sentence in raw_sentences)
+
+        phrases = Phrases(tokenized_sentences)
+        bigram = Phraser(phrases)
+
+        sentences = list(bigram[tokenized_sentences])
+
+        return sentences
+
+    def train(self, sentences=None):
+        start = time.time()
+
+        if sentences is None:
+            sentences = self.get_sentences()
+
+        trained = self.model(sentences=sentences, window=self.window, min_count=self.min_count, workers=self.workers,
+                             sample=self.sample, negative=self.negative, seed=self.seed)
+
+        trained.save(self.out)
+        print("Training {0} took: {1} seconds".format(self.model_name, time.time()-start))
---
+++ train.py
@@ -0,0 +1,36 @@
+import sys, multiprocessing
+from .WordEmbeddings import WordEmbeddings
+
+models = {"{0}_{1}".format(model, variation) for model in ["w2v", "ft"] for variation in {"sg","cbow"}}
+
+if __name__ == '__main__':
+    program = sys.argv[0]
+    inp = sys.argv[1]
+    out = sys.argv[2]
+    try:
+        model = sys.argv[3]
+        if model not in models:
+            print("Model not specified, using FastText Skip-Gram by default")
+            raise IndexError
+    except IndexError:
+        print("Model not specified, using FastText Skip-Gram by default")
+
+        model = "ft_sg"
+
+
+    window = int(input("Window size (default 300): ") or 300)
+    min_word_count = int(input("min_word_count (default 3): ") or 3)
+    workers = int(input("Workers (default all): ") or multiprocessing.cpu_count())
+    subsampling = float(input("Subsampling ratio (default 1e-3): ") or 0.001)
+    negative = int(input("Negative samples (default 5): ") or 5)
+    seed = int(input("random_seed (default 48): ") or 48)
+
+    we = WordEmbeddings(inp, out,
+                        model_name=model,
+                        window=window,
+                        min_count=min_word_count,
+                        workers=workers,
+                        sample=subsampling,
+                        negative=negative,
+                        seed=seed)
+    we.train()
