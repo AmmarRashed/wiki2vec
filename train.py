67d04369e8e3349c9ce1b75e917c5a6d90b00904
Ammar Rashed <ammarrashed@std.sehir.edu.tr>
2018-07-06 15:23:41 +0300

    changed cli arguments text

--- train.py
+++ train.py
@@ -1,31 +1,29 @@
 import argparse, multiprocessing
 from WordEmbeddings import WordEmbeddings
 
-models = {"{0}_{1}".format(model, variation) for model in ["w2v", "ft"] for variation in {"sg","cbow"}}
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("i",help="path to the input bz2 wiki dump")
-    parser.add_argument("o",help="path to store the trained model in")
-    parser.add_argument("--ft", help="Use FastText (default 1)", default=1, type=int)
-    parser.add_argument("--sg", help="Use SkipGram (default 1)", default=1, type=int)
-    parser.add_argument("--window", help="context window size (default 300)", default=300, type=int)
-    parser.add_argument("--min_word_count", help="min word count (default 3)", default=3, type=int)
-    parser.add_argument("--workers", help="utilized cores count (default all)", default=multiprocessing.cpu_count(), type=int)
-    parser.add_argument("--sample", help="subsampling ratio (default 0.001)", default=0.001, type=float)
-    parser.add_argument("--negative", help="negative samples count (default 5)", default=5, type=int)
-    parser.add_argument("--seed", help="random seed (default 48)", default=48, type=int)
+    parser.add_argument("i",help="input (bz2 wiki dump) path")
+    parser.add_argument("o",help="output (trained model) path")
+    parser.add_argument("--ft", help="use FastText (bool: default True)", default=True, type=bool)
+    parser.add_argument("--sg", help="use SkipGram (bool: default True)", default=True, type=bool)
+    parser.add_argument("--window", help="context window size (int: default 300)", default=300, type=int)
+    parser.add_argument("--min_word_count", help="min word count (int: default 3)", default=3, type=int)
+    parser.add_argument("--workers", help="utilized cores count (int: default all)", default=multiprocessing.cpu_count(), type=int)
+    parser.add_argument("--sample", help="subsampling ratio (float: default 0.001)", default=0.001, type=float)
+    parser.add_argument("--negative", help="negative samples count (int: default 5)", default=5, type=int)
+    parser.add_argument("--seed", help="random seed (int: default 48)", default=48, type=int)
 
     args = parser.parse_args()
 
     inp = args.i
     out = args.o
-    if int(args.ft):
+    if args.ft:
         model = "ft_"
     else:
         model = "w2v_"
 
-    if int(args.sg):
+    if args.sg:
         model += "sg"
     else:
         model += "cbow"
